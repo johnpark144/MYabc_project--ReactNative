aa073c492fab169e7970cb7afbabc57ae2e77fa3
Yeonghwan Park <vyckd354@gmail.com>
2023-08-05 01:30:39 +0900

    formed some frames of 'Video' in Home

--- components/home/Greeting.jsx
+++ components/home/Greeting.jsx
@@ -1,7 +1,9 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import React, { useContext } from 'react';
 import AuthContext from '../../context/AuthContext';
 import { pTSansNarrowBold, sairaCondensedLight } from '../../commonStyles';
+import { Linking } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const Greeting = () => {
   let { user, loginUser, error, checkAuthState, logoutUser } =
@@ -16,7 +18,7 @@ const Greeting = () => {
             source={{
               uri: 'https://user-images.githubusercontent.com/106279616/217299245-76306248-6c80-4bf8-a1f0-ccb962648a8f.png',
             }}
-            className='w-56 h-56'
+            className='w-44 h-44'
           />
         </View>
         <View className='gap-y-5'>
@@ -34,6 +36,24 @@ const Greeting = () => {
             <Text>Feel free to use it!{'\n'}</Text>
             <Text>Application maker : John park (박영환)</Text>
           </Text>
+          <View className='flex-row justify-center w-full'>
+            <LinearGradient
+              className='w-2/3 h-10 rounded-3xl flex-row justify-center items-center shadow-xl shadow-black'
+              start={{ x: 0, y: 1 }}
+              end={{ x: 1, y: 0 }}
+              colors={['#52e4e0', '#4ce2b6']}
+            >
+              <TouchableOpacity
+                onPress={() =>
+                  Linking.openURL(
+                    'https://resilient-hummingbird-f7421f.netlify.app/'
+                  )
+                }
+              >
+                <Text>Web Version</Text>
+              </TouchableOpacity>
+            </LinearGradient>
+          </View>
         </View>
       </View>
     </>
--- components/home/Vid.jsx
+++ components/home/Vid.jsx
@@ -1,17 +1,85 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, Image } from 'react-native';
+import React, { useRef, useState } from 'react';
 import { ImageBackground } from 'react-native';
+import FontText from '../CommonFontText';
+import { pTSansNarrowBold } from '../../commonStyles';
+import { ResizeMode, Video } from 'expo-av';
+import { Pressable } from 'react-native';
 
 const Vid = () => {
+  const videoRef = useRef(null);
+  const [isPaused, setIsPaused] = useState(true);
+
+  const handleImageClick = () => {
+    try {
+      setIsPaused((prev) => !prev);
+    } catch (error) {
+      console.error('Error while pausing the video:', error);
+    }
+  };
+
   return (
     <ImageBackground
-      className='w-screen h-screen p-8 gap-y-3'
+      className='w-screen h-screen px-6 py-4 gap-y-8'
       source={{
         uri: 'https://user-images.githubusercontent.com/106279616/257908892-ebac26ef-b392-4a8b-81a3-59fd95b66b2e.jpg',
       }}
       resizeMode='cover'
     >
-      <View></View>
+      <View>
+        {/* 설명 */}
+        <FontText className='text-2xl leading-10 bg-[#75c3db] rounded-3xl p-6 shadow-md shadow-black'>
+          <Text style={pTSansNarrowBold} className='text-4xl'>
+            VIDEO {'\n'}
+          </Text>
+          <Text>Search any words to see {'\n'}</Text>
+          <Text>how often they were used in Youtube, {'\n'}</Text>
+          <Text>and in what context they were used, {'\n'}</Text>
+          <Text>and create some words you will memorize</Text>
+        </FontText>
+      </View>
+      <View className='w-full flex-row justify-center'>
+        {/* 비디오 프레임 */}
+        <Image
+          className='absolute w-[300px] h-[200px]'
+          source={{
+            uri: 'https://user-images.githubusercontent.com/106279616/258457106-0859463d-90f0-4d72-a53e-5ee23635016c.jpg',
+          }}
+          resizeMode='stretch'
+        />
+        {/* 비디오 */}
+        <Pressable // 버튼 효과 내 줄 수있는 또 다른 옵션
+          className='absolute'
+          onPress={handleImageClick}
+        >
+          <Video
+            ref={videoRef}
+            className='w-[300px] h-[162px]'
+            resizeMode={ResizeMode.STRETCH}
+            source={{
+              uri: 'https://user-images.githubusercontent.com/106279616/217296607-79dcf640-b492-4cbb-805c-7515ceba2de0.mp4',
+            }}
+            isLooping={true}
+            shouldPlay={!isPaused}
+            blurRadius={0}
+          />
+        </Pressable>
+        {/* 재생버튼 */}
+        {isPaused && (
+          <Pressable // 버튼 효과 내 줄 수있는 또 다른 옵션
+            className='w-[300px] h-[162px] flex-row justify-center items-center bg-[#000000aa]' // blur 효과
+            onPress={handleImageClick}
+          >
+            <Image
+              className='w-[50px] h-[50px]'
+              source={{
+                uri: 'https://user-images.githubusercontent.com/106279616/258436789-d722fcfd-ba9a-4d25-9f13-a140deca316e.png',
+              }}
+              resizeMode='stretch'
+            />
+          </Pressable>
+        )}
+      </View>
     </ImageBackground>
   );
 };
